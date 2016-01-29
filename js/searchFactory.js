1b2e1e3a0727fdc864ac4f05e35741dcc5dbaefc
charlieperson <charlie.person@hyperisland.se>
2016-01-29 11:35:13 +0000

    Made access token available for heroku

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,5 +1,7 @@
 githubUserSearch.factory('Search', ['$http', function($http){
+  var access_token = 'a70e53ac1ea367cad16c02891f09ea8ba3f2d2a3';
   var queryUrl = 'https://api.github.com/search/users';
+  
   return {
     query: function(searchTerm) {
       return $http({
